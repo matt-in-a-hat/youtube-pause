9a8d9dc47bc6a7e1c873c7622dc210f8b9713d4a
Matt Lang <matt@mediasuite.co.nz>
2013-09-18 00:12:48 +1200

    Adds some comments/reminders.

--- contentscript.js
+++ contentscript.js
@@ -5,6 +5,13 @@
 
     var videoElement = document.getElementById('movie_player');
 
+    // This doesn't work. It should. But wont. Bastards.
+    // videoElement.addEventListenter('onStateChange', function (event) {});
+    // Instead perhaps I'll have to have all currently active youtube tabs
+    //  polling videoElement.getPlayerState() to test state changes. FML.
+    // And/or, reimplement my force-HTML5-hack and see if I can access those events.
+    //  Note: If I do re-introduce the hack, at least fix back-browsing.
+
     chrome.runtime.onMessage.addListener(function (message, sender, callback) {
         if (message === "pause") {
             videoElement.pauseVideo();
