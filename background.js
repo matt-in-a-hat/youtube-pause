64576e2605c220354fe9d71d3c48e9c334c3bb25
Matt Lang <matt@mediasuite.co.nz>
2013-10-07 22:31:25 +1300

    All (other) youtube tabs now pause whenever a youtube tab's video starts/unpauses. Specifically:
    Updated content scripts to message background script to register instead of bg also matching urls.
    Updated content script to message bg script on video state change.
    Updated bg script to broadcast "pause" to all registered tabs when a video starts playing (except the tab that started playing).
    Changed to 2 space.
    Added .editorconfig and .gitignore

--- background.js
+++ background.js
@@ -1,64 +1,90 @@
-/*jslint indent:4*/
+/*jslint indent:2*/
 /*global chrome*/
 
 // This script is run when the addon is loaded by the browser (probably when the first window opens).
 (function () {
 
-    var urlIndex = {};
-    var youtubeTabIds = {};
-    var activeYoutubeTabId;
+  // var urlIndex = {};
+  // var youtubeTabIds = {};
+  // var activeYoutubeTabId;
 
-    var isYoutubeURL = function (url) {
-        return url.match(/^(http[s]?:\/\/)?www\.youtube\.com\/watch/) !== null;
-    };
+  // var isYoutubeURL = function (url) {
+  //   return url.match(/^(http[s]?:\/\/)?www\.youtube\.com\/watch/) !== null;
+  // };
 
-    var onNewRequest = function (tab) {
-        var url = tab.url;
-        if (isYoutubeURL(url)) {
-            if (urlIndex[url] === undefined) {
-                urlIndex[url] = {
-                    tabIds: []
-                };
-                // var namesDiv = document.getElementById('video_urls');
-                // var p = document.createElement('p');
-                // var content = document.createTextNode(url);
-                // p.appendChild(content);
-                // namesDiv.appendChild(p);
-                // urlIndex[url].htmlItem = p;
+  // var onNewRequest = function (tab) {
+  //   var url = tab.url;
+  //   if (isYoutubeURL(url)) {
+  //     if (urlIndex[url] === undefined) {
+  //       urlIndex[url] = {
+  //         tabIds: []
+  //       };
+  //       // var namesDiv = document.getElementById("video_urls");
+  //       // var p = document.createElement("p");
+  //       // var content = document.createTextNode(url);
+  //       // p.appendChild(content);
+  //       // namesDiv.appendChild(p);
+  //       // urlIndex[url].htmlItem = p;
+  //     }
+
+  //     // urlIndex[url].htmlItem.classList.add("tab-open");
+
+  //     activeYoutubeTabId = tab.id;
+  //     var tabId;
+  //     // for (tabId in youtubeTabIds) {
+  //     Object.keys(youtubeTabIds).forEach(function (tabId) {
+  //       var match = tabId.match(/^(\d+)_tabId$/);
+  //       if (match !== null) {
+  //         tabId = parseInt(match[1], 10);
+  //         if (tabId !== tab.id) {
+  //           chrome.tabs.sendMessage(tabId, "pause");
+  //         }
+  //       }
+  //     });
+
+  //     urlIndex[url].tabIds.push(tab.id);
+  //     youtubeTabIds[tab.id + "_tabId"] = true;
+  //   }
+  // };
+
+  // var onTabClose = function (tabId) {
+
+  // };
+
+  // chrome.tabs.onCreated.addListener(function (tab) {
+  //   onNewRequest(tab);
+  // });
+  // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  //   onNewRequest(tab);
+  // });
+  // chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+  //   console.log("Removed", removeInfo);
+  // });
+
+
+  var youtubeTabIds = {};
+
+  chrome.runtime.onMessage.addListener(function (request, sender, callback) {
+    console.log("background received message", request, sender, callback);
+    var responseObj;
+    if (request.message === "registerYoutubeTab") {
+      youtubeTabIds[sender.tab.id + "_tabId"] = true;
+    } else if (request.message === "videoStateChange") {
+      if (request.videoState === 1) {
+        Object.keys(youtubeTabIds).forEach(function (tabId) {
+          var match = tabId.match(/^(\d+)_tabId$/);
+          if (match !== null) {
+            tabId = parseInt(match[1], 10);
+            if (tabId !== sender.tab.id) {
+              chrome.tabs.sendMessage(tabId, {message: "pauseVideo"});
             }
-
-            // urlIndex[url].htmlItem.classList.add('tab-open');
-
-            activeYoutubeTabId = tab.id;
-            var tabId;
-            // for (tabId in youtubeTabIds) {
-            Object.keys(youtubeTabIds).forEach(function (tabId) {
-                var match = tabId.match(/^(\d+)_tabId$/);
-                if (match !== null) {
-                    tabId = parseInt(match[1], 10);
-                    if (tabId !== tab.id) {
-                        chrome.tabs.sendMessage(tabId, "pause");
-                    }
-                }
-            });
-
-            urlIndex[url].tabIds.push(tab.id);
-            youtubeTabIds[tab.id + '_tabId'] = true;
-        }
-    };
-
-    var onTabClose = function (tabId) {
-
-    };
-
-    chrome.tabs.onCreated.addListener(function (tab) {
-        onNewRequest(tab);
-    });
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        onNewRequest(tab);
-    });
-    chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-        console.log('Removed', removeInfo);
-    });
+          }
+        });
+      }
+    }
+    if (typeof callback === "function") {
+      callback(responseObj);
+    }
+  });
 
 }());
--- contentscript.js
+++ contentscript.js
@@ -1,33 +1,45 @@
-/*jslint indent:4*/
+/*jslint indent:2*/
 /*global window, document, chrome*/
 
 // This is the script injected into all pages matching youtube as defined in manifest.json
 (function () {
 
-    var videoElement = document.getElementById('movie_player');
+  var videoElement = document.getElementById("movie_player");
 
-    // This doesn't work. It should. But wont. Bastards.
-    // videoElement.addEventListenter('onStateChange', function (event) {});
-    // Instead perhaps I'll have to have all currently active youtube tabs
-    //  polling videoElement.getPlayerState() to test state changes. FML.
-    // And/or, reimplement my force-HTML5-hack and see if I can access those events.
-    //  Note: If I do re-introduce the hack, at least fix back-browsing.
-    
-    var videoState = null;
-    var broadcastVideoPlayerState = function () {
-        var newState = videoElement.getPlayerState();
-        if (newState !== videoState) {
-            videoState = newState;
-            chrome.tabs.sendMessage(tabId, "pause");
-        }
+  // This doesn't work. It should. But wont. Bastards.
+  // videoElement.addEventListenter("onStateChange", function (event) {});
+  // Instead perhaps I'll have to have all currently active youtube tabs
+  //  polling videoElement.getPlayerState() to test state changes. FML.
+  // And/or, reimplement my force-HTML5-hack and see if I can access those events.
+  //  Note: If I do re-introduce the hack, at least fix back-browsing.
+
+  var videoState = null;
+  var broadcastVideoPlayerState = function () {
+    var newState = videoElement.getPlayerState();
+    if (newState !== videoState) {
+      videoState = newState;
+      var message = {
+        message: "videoStateChange",
+        videoState: newState
+      };
+      chrome.runtime.sendMessage(message, function (response) {
+        console.log("Response", response);
+      });
     }
+  };
 
-    var poller = window.setInterval(broadcastVideoPlayerState, 500);
+  var poller = window.setInterval(broadcastVideoPlayerState, 250);
 
-    chrome.runtime.onMessage.addListener(function (message, sender, callback) {
-        if (message === "pause") {
-            videoElement.pauseVideo();
-        }
-    });
+  chrome.runtime.onMessage.addListener(function (request, sender, callback) {
+    console.log("content script received message", request, sender, callback);
+    if (request.message === "pauseVideo") {
+      videoElement.pauseVideo();
+    }
+  });
+
+
+  chrome.runtime.sendMessage({message: "registerYoutubeTab"}, function (response) {
+    console.log("Response to register", response);
+  });
 
 }());
